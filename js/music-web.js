ef4b30c871f132b12a14f817c8346c1179d1d36b
Siv Ratha <siv.ratha89@gmail.com>
2015-02-19 14:46:33 +0700

    version 1.2.0 ----------------------------- Big Update

---
+++ js/music-web.js
@@ -0,0 +1,22 @@
+/**
+ * Created by rathaROG on 2/16/2015.
+ */
+
+$("document").ready(function() {
+
+    /*Start: Prevent the default white background on blur of top navigation */
+    $(".dropdown-menu li a").mousedown(function() {
+        var dropdown = $(this).parents('.dropdown');
+        var link = dropdown.children(':first-child');
+        link.css('background-color', "#2E3436");
+        link.css('color', 'white');
+    });
+    /*End: Prevent the default white background on blur of top navigation */
+
+    /*Start : Automatically start the slider */
+    $('.carousel').carousel({
+        interval: 4000
+    });
+    /*End: Automatically start the slider */
+
+});
